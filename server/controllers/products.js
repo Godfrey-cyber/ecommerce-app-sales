d2e9f9d26195bf824bf3d9661a09dc9cb8a07a37
Godfrey-cyber <godfreyndiritu26@gmail.com>
2026-01-02 14:42:31 +0300

    implemented slug, check for slug, catId validation

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,4 +1,6 @@
 import Products from '../models/Products.js'
+import slugify from 'slugify'
+import mongoose from "mongoose"
 
 export const createProduct = async(req, res) => {
     const { name, desc, price, image, quantity, discount, catId, userId, brand, condition } = req.body
@@ -6,8 +8,28 @@ export const createProduct = async(req, res) => {
         return res.status(400).json({ msg: '❌ Please enter all fields' })
     }
 
+    if (!req.userId) {
+      return res.status(400).json("Unauthorized");
+    }
+    const slug = slugify(title, { lower: true })
+    // @Check for duplicate slug
+    const existingSlug = await Products.findOne({ slug })
+    if (existingSlug) {
+      return res.status(409).json({ msg: "🚫 A blog with this title already exists" })
+    }
+    // @Check for duplicate category
+    const existingCategory = await Category.findById(catId);
+    if (!existingCategory) {
+      return res.status(400).json("Category does not exist");
+    }
+
+    // @validate ObjectId format
+    if (!mongoose.Schema.Types.ObjectId.isValid(catId)) {
+        return res.status(400).json({ msg: '❌ Invalid category Id' })
+    }
+
     try {
-        const product = await Products.create({ name, desc, price, image, quantity, discount, catId, userId, brand, condition })
+        const product = await Products.create({ name, desc, price, image, quantity, discount, catId, userId, brand, condition, slug })
             // await Category.findByIdAndUpdate(catId, {$push:{productId: product._id }})
         return res.status(201).json({data: product, status: 'Success', statusText: "ok", statusCode: 201 })
     } catch (error) {
