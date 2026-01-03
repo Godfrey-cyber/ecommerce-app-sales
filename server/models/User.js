c67ce75ec32cf741e17b3d28c5c9e2e1276cf854
Godfrey-cyber <godfreyndiritu26@gmail.com>
2026-01-03 14:23:41 +0300

    removes required true in the firstname and lastname

--- server/models/User.js
+++ server/models/User.js
@@ -3,11 +3,12 @@ import { Schema } from "mongoose"
 import bcrypt from "bcryptjs"
 
 const UserSchema = new mongoose.Schema({
-    firstname: { type: String, unique: true, min: 4, required: true },
-	lastname: { type: String, unique: true, min: 4, required: true },
+    firstname: { type: String, min: 4, required: true },
+	lastname: { type: String, min: 4, required: true },
     password: { type: String, required: true, select: false },
 	email: { type: String, required: true },
     role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    refreshTokens: [{ token: String, createdAt: { type: Date, default: Date.now } }],
     verified: { type: Boolean, default: false }
 }, { timestamps: true })
 
