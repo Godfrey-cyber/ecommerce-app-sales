77a2033463e1b47a06514f87179e26fcd04b0eb7
Godfrey-cyber <godfreyndiritu26@gmail.com>
2026-01-05 07:34:36 +0300

    run the sctript to delete duplicate name_1

--- server/utilities/scripts.js
+++ server/utilities/scripts.js
@@ -7,14 +7,14 @@ async function dropIndex() {
     await mongoose.connect("mongodb+srv://wave-ecommerce:com-shop-0860@cluster0.vqtnd.mongodb.net/commerce-sale?retryWrites=true&w=majority"); // replace with your Mongo URI
     const db = mongoose.connection.db;
 
-    const indexes = await db.collection('users').indexes();
+    const indexes = await db.collection('products').indexes();
     console.log('Existing indexes:', indexes);
 
-    if (indexes.some(idx => idx.name === 'username_1')) {
-      await db.collection('users').dropIndex('username_1');
-      console.log('Dropped username_1 index successfully');
+    if (indexes.some(idx => idx.name === 'name_1')) {
+      await db.collection('products').dropIndex('name_1');
+      console.log('Dropped name_1 index successfully');
     } else {
-      console.log('No username_1 index found');
+      console.log('No name_1 index found');
     }
 
     await mongoose.disconnect();
@@ -26,4 +26,4 @@ async function dropIndex() {
 dropIndex();
 
 
-// Dropped username_1 index
+// Dropped username_1 index  run node utilities/scripts.js
