1b1afcfe822b018294f5e109d6d52302ec4d7240
Godfrey-cyber <godfreyndiritu26@gmail.com>
2026-01-12 09:54:41 +0300

    minor adjustments

--- server/index.js
+++ server/index.js
@@ -42,7 +42,6 @@ app.use((req, res, next) => {
 app.use("/v1/api/users", userRoutes);
 app.use("/v1/api/products", productRoutes);
 app.use("/v1/api/categories", categoryRoutes);
-console.log(process.env.CLIENT_URL, PORT)
 
 app.use((req, res) => {
   res.status(404).json({ msg: "Route not found" });
