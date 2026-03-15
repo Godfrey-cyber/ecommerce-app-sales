8c21ddfb614740a0f3c0832b5d680392572d7f86
Godfrey <godfreyndiritu26@gmail.com>
2026-03-15 18:38:19 +0300

    fix: exclude /assets from Vercel catch-all rewrite and deleted gitlab-ci.yml

---
+++ client/gpush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "❌ Please provide a commit message"
+  echo 'Usage: ./gpush.sh "your commit message"'
+  exit 1
+fi
+
+git add .
+
+git commit -m "$1"
+
+git push
