9336ba0e52010051bc66e522ed6c5e8c3fe33a8e
Godfrey <godfreyndiritu26@gmail.com>
2026-03-16 11:19:08 +0300

    Fix: protect add-to-cart route with auth middleware Only logged in users can add items to cart. Unauthenticated requests are rejected with a 401.

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,9 +3,9 @@ import { addToCart, getCart, getOne, updateCartItem, removeCartItem } from "../c
 import { authenticate } from "../utilities/authMiddleware.js"
 const router = express.Router()
 
-router.post("/add-to-cart", addToCart);
-router.put("/update-cart-item/:itemId", updateCartItem);
-router.delete("/remove-cart-item/:itemId", removeCartItem);
+router.post("/add-to-cart", authenticate, addToCart);
+router.put("/update-cart-item/:itemId", authenticate, updateCartItem);
+router.delete("/remove-cart-item/:itemId", authenticate, removeCartItem);
 // router.delete("/clear-cart/:cartId", clearCart);
 router.get("/get-cart", authenticate, getCart);
 router.get("/get-one", getOne);
