39f24d361f6563c26edc2b2005cb93d90c29b7a3
Godfrey <godfreyndiritu26@gmail.com>
2026-05-18 15:42:00 +0300

    feat (order) changed the payment api to submit orderId as a string and not as an object

--- client/src/redux/paymentApi.jsx
+++ client/src/redux/paymentApi.jsx
@@ -3,17 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const paymentApi = createApi({
   	reducerPath: "paymentApi",
   	baseQuery: fetchBaseQuery({
-	    baseUrl: "/api/payments",
+	    baseUrl: import.meta.env.VITE_API_URL,
 	    credentials: "include", // sends HTTP-only auth cookie
   	}),
   	endpoints: (builder) => ({
  
 	    // Creates a PaymentIntent on the backend → returns { clientSecret }
 	    createPaymentIntent: builder.mutation({
-	      	query: ({ cartItems }) => ({
-	        	url: "/create-payment-intent",
+	      	query: ({ orderId }) => ({
+	        	url: "payments/stripe/create-intent",
 	        	method: "POST",
-	        	body: cartItems,
+	        	body: { orderId },
 	      	}),
 	    }),
   	}),
