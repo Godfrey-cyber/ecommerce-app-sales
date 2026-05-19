d109ebd08fbeaec5b4abae318a49165586fb9192
Godfrey <godfreyndiritu26@gmail.com>
2026-05-19 11:59:50 +0300

    Package.json - reinstalled all the packages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: "true",
-    proxy: {
-      '/v1/api': 'http://api:8080'
+    plugins: [react()],
+    server: {
+        port: 5173,
+        host: "true",
+        proxy: {
+            '/v1/api': 'http://api:8080'
+        }
     }
-  }
 })
